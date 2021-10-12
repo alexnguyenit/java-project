d965b03e8e45e68780aba239dc1c060c7166b986
Hoang Nguyen <nnhoang@cmc.com.vn>
2021-10-12 15:52:00 +0700

    fox

---
+++ src/test/java/com/boby/JavaProjectCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
